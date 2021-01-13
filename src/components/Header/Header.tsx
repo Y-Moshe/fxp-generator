23417c426068648b03d366572f043607ac5b4fbc
Y_Moshe <moshe.nehemiah@gmail.com>
2021-01-13 16:59:53 +0200

    gh-pages

--- src/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-    return (
-        <header>
-            <h1>FxP Generator - כלי לייצור קוד להכרזות והודעות פרטיות</h1>
-        </header>
-    );
-}
