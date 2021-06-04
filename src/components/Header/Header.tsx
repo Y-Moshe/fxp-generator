c34594a01a305eea1a96ab92f147b120951e0523
y-moshe <moshe.nehemiah@gmail.com>
2021-06-04 17:11:13 +0300

    Responsive fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,16 +5,27 @@ import { version } from '../../../package.json';
 export default function Header() {
     return (
         <header className = { classes.AppHeader }>
-            <h2 className = { classes.AppVersion }> FxP Generator v{version} -
-                <a
-                    href   = "https://github.com/Y-Moshe/fxp-generator"
-                    target = "_blank"
-                    rel    = "noreferrer">
-                    <img src = "https://i.imagesup.co/images2/fabe53eb72f9b6d3d47cd95aff31ffc45c2fdbf8.png" alt = "Github"/>
-                </a>
-            </h2>
+            <img
+                src="https://images.weserv.nl/?url=i.imgur.com/DPZXQRv.png"
+                alt="fxp logo"
+                className = { classes.Logo } />
 
-            <h2> כלי לייצור קוד להכרזות, הודעות פרטיות, אזהרות ועוד. </h2>
+            <main className = { classes.AppInfo }>
+                <h2 className = { classes.AppVersion }> FxP Generator v{version} -
+                    <a
+                        href   = "https://github.com/Y-Moshe/fxp-generator"
+                        target = "_blank"
+                        rel    = "noreferrer">
+                        <img src = "https://i.imagesup.co/images2/fabe53eb72f9b6d3d47cd95aff31ffc45c2fdbf8.png" alt = "Github"/>
+                    </a>
+                </h2>
+
+                <h2> כלי לייצור קוד להכרזות, הודעות פרטיות, אזהרות ועוד. </h2>
+            </main>
+
+            <div className = { classes.UserSettings }>
+
+            </div>
         </header>
     );
 }
